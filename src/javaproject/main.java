ba3606b1906263924bf2a7e9fd6a9ac147b46112
msharma1409740052 <154513631+msharma1409740052@users.noreply.github.com>
2023-12-29 12:25:29 +0530

    initial commit

---
+++ src/javaproject/main.java
@@ -0,0 +1,19 @@
+package javaproject;
+
+public class main {
+
+	
+ public static void main(String[] args) {
+	
+	 
+	 System.out.println("hello mohit.................");
+	 
+	 System.out.println("hello shailendra.................");
+	 System.out.println("hello ravi.................");
+	 System.out.println("hello abc.................");
+	 System.out.println("hello xyz.................");
+	 System.out.println("hello akshay.................");
+
+}
+	
+}
